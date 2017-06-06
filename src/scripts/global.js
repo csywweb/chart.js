e1fc5d19bfbf378894ac86493afe945cfd906049
xjcjcsy <xjcjcsy@sina.cn>
2017-06-07 01:41:03 +0800

    feat: 修改画布内容位置

--- src/scripts/global.js
+++ src/scripts/global.js
@@ -44,6 +44,25 @@
    *
    */
   utils = {
+    isObject : function(obj){
+      return obj !== null && typeof obj === 'object' && Object.getPrototypeOf(obj) === Object.prototype;
+    },
+    extend : function(target, obj){
+      for(var k in obj) {
+        var src = target[k];
+        var copy = obj[k];
+        if(src === copy) {
+            continue;
+        }
+        if(this.isObject(copy)) {
+            src = src || {};
+            target[k] = this.extend(src, copy);
+        } else {
+            target[k] = copy;
+        }
+      }
+      return target;
+    },
     getNum : function(string){
       var value = string.replace(/[^0-9]/ig,""); 
       return value;
@@ -85,14 +104,32 @@
   }
   function CreateChart(selector){
     this.target = document.getElementById(selector);
-    
+    // 默认配置项
+    this.config = {
+      color : {
+        extendsLine : "#DDD",
+        Axis : "#333",
+        line : "#FF0000",
+        fontColor : "#333"
+      },
+      isX : true,
+      isY : true
+    }
+
+    //生成canvas对象
+    var canvas  = document.createElement('canvas');
+    this.canvas = canvas;
+    this.ctx    = canvas.getContext("2d");
+    //设备缩放比
+    var radio           = utils.getPixelRatio(this.ctx);
+    this.radio          = radio;
+
     var obj = this.target, style;
     if(window.getComputedStyle){
       style = window.getComputedStyle(obj, null);
     } else {
       style = obj.currentStyle;
     }
-
     //宽度高度， 带单位px
     this.width  = utils.getNum(style.width) - utils.getNum(style.paddingLeft) - utils.getNum(style.paddingRight) + "px";
     this.height = utils.getNum(style.height) - utils.getNum(style.paddingTop) - utils.getNum(style.paddingBottom) + "px";
@@ -101,14 +138,6 @@
     this.width_num  = utils.getNum(this.width); 
     this.height_num = utils.getNum(this.height);
 
-    //生成canvas对象
-    var canvas  = document.createElement('canvas');
-    this.canvas = canvas;
-    this.ctx    = canvas.getContext("2d");
-
-    //设备缩放比
-    var radio           = utils.getPixelRatio(this.ctx);
-    this.radio          = radio;
     //定义样式
     canvas.width        = this.width_num * radio;
     canvas.height       = this.height_num * radio;
@@ -122,28 +151,17 @@
   *
   */
   CreateChart.prototype.setOption = function(options){
-    this.options      = options;
-    this.xAxis_data   = options.xAxis.data;
-    this.yAxis_data   = options.yAxis.data;
-    this.yLength      = this.yAxis_data.length;
-    this.xLength      = this.xAxis_data.length;     //x轴数据的长度
-    var points        = this.xLength + 1;
-    this.xAxisUnit    = parseInt(this.width_num / points);  //y轴单位刻度
+    this.options  = utils.extend(this.config, options);
     
-    if(this.xLength > this.yLength){
-      this.xLength = this.yLength;
-    } else if(this.xLength < this.yLength){
-      this.yLength = this.xLength;
+    this.initAxis();
+    if(this.xSize > this.yLength){
+      this.xSize = this.yLength;
+    } else if(this.xSize < this.yLength){
+      this.yLength = this.xSize;
     }
-
-
 
     var canvas = this.canvas;
     this.target.appendChild(canvas);
-    
-    //坐标原点
-    this.startX = 10;
-    this.startY = this.height_num*this.radio - 30;
 
     this.setXAxis();      //设定x轴的下标
     this.lineXAxis();     //x轴对应下标的延长线
@@ -151,57 +169,44 @@
     this.drawYAxix();     //绘制y轴
     this.drawLine();      //绘制折线
   }
+  CreateChart.prototype.initAxis = function(){
+    //坐标原点
+    this.startX = 30
+    this.startY = (this.height_num)*this.radio - 30;
+    //x轴的最大坐标点
+    this.x_max = (this.width_num)*this.radio - 30;
+    //x轴长度
+    this.x_length = this.x_max - this.startX;
+
+    this.xAxis_data   = this.options.xAxis.data;
+    this.yAxis_data   = this.options.yAxis.data;
+    this.yLength      = this.yAxis_data.length;                         //y轴数据的个数
+    this.xSize        = this.xAxis_data.length;                         //x轴数据的个数
+    this.xAxisUnit    = parseInt(this.x_length / (this.xSize - 1));  //x轴单位刻度
+    console.log(this)
+  }
   CreateChart.prototype.setXAxis = function(){
-    var length = this.xLength;
+    var length = this.xSize;
     var unit   = this.xAxisUnit;
     
-    this.ctx.fillStyle    = "#666";
+    this.ctx.fillStyle    = this.options.color.fontColor;
     this.ctx.font         = 12*this.radio + "px" +" PingFang SC";
     this.ctx.textAlign    = "center";
     for(var i = 0; i < length; i++){
-      var x = unit*(i+1)*this.radio;
+      var x = this.startX + unit*(i);
       var y = this.height_num*this.radio - 5;
      
       this.ctx.fillText(this.xAxis_data[i], x, y);
      
     }
   }
-  CreateChart.prototype.drawXAxis = function(){
-    var ctx    = this.ctx;
-    var startX = this.startX;
-    var startY = this.startY;
-    var endX   = this.width_num*this.radio;
-    ctx.beginPath();
-    ctx.moveTo(startX, startY);
-    ctx.lineTo(endX, startY);
-    ctx.lineTo(endX - 10, startY - 6);
-    ctx.moveTo(endX, startY);
-    ctx.lineTo(endX - 10, startY + 6);
-    ctx.lineJoin    = 'round';
-    ctx.strokeStyle = "#333";
-    ctx.stroke();
-  }
-
-  CreateChart.prototype.drawYAxix = function(){
-    var ctx = this.ctx;
-    ctx.beginPath();
-    ctx.moveTo(this.startX, this.startY);
-    ctx.lineTo(this.startX, 0);
-    ctx.lineTo(this.startX + 6, 10);
-    ctx.moveTo(this.startX, 0);
-    ctx.lineTo(this.startX - 6, 10);
-    ctx.lineJoin = 'round';
-    ctx.strokeStyle = "#333";
-    ctx.stroke();
-  }
-
   CreateChart.prototype.lineXAxis = function(){
     var xAxis_data = this.options.xAxis.data;
     var length     = this.xAxis_data.length;
     var unit       = this.xAxisUnit;
 
     for(var i = 0; i < length; i++){
-      var x = unit*(i+1)*this.radio;
+      var x = this.startX + unit*(i);
       var y = this.height_num*this.radio - 5;
      
       this.ctx.beginPath();
@@ -212,9 +217,39 @@
       this.ctx.stroke();
     }
   }
+  CreateChart.prototype.drawXAxis = function(){
+    var ctx    = this.ctx;
+    var startX = this.startX;
+    var startY = this.startY;
+    var endX   = this.x_max;
+    ctx.beginPath();
+    ctx.moveTo(startX, startY);
+    ctx.lineTo(endX, startY);
+    // ctx.lineTo(endX - 10, startY - 6);
+    // ctx.moveTo(endX, startY);
+    // ctx.lineTo(endX - 10, startY + 6);
+    ctx.lineJoin    = 'round';
+    ctx.strokeStyle = "#333";
+    ctx.stroke();
+  }
+
+  CreateChart.prototype.drawYAxix = function(){
+    var ctx = this.ctx;
+    ctx.beginPath();
+    ctx.moveTo(this.startX, this.startY);
+    ctx.lineTo(this.startX, 0);
+    // ctx.lineTo(this.startX + 6, 10);
+    // ctx.moveTo(this.startX, 0);
+    // ctx.lineTo(this.startX - 6, 10);
+    ctx.lineJoin = 'round';
+    ctx.strokeStyle = "#333";
+    ctx.stroke();
+  }
+
+  
   CreateChart.prototype.drawLine = function(){
     var ydata = this.yAxis_data;
-    
+
 
 
   }
